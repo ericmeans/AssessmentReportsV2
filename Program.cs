a91ea124bf264f33b04ccd8b71a94fb13892e257
ericmeans <eric@randomtree.org>
2018-08-20 20:28:21 -0600

    Fix error in argument counting

--- Program.cs
+++ Program.cs
@@ -19,32 +19,32 @@ namespace AssessmentReportsV2
             };
 
             var index = args.IndexOf("-file");
-            if (index >= 0 && args.Length > index + 2)
+            if (index >= 0 && args.Length >= index + 2)
             {
                 options.Filename = args[index + 1];
             }
             index = args.IndexOf("-semester");
-            if (index >= 0 && args.Length > index + 2)
+            if (index >= 0 && args.Length >= index + 2)
             {
                 options.CurrentSemester = args[index + 1];
             }
             index = args.IndexOf("-sheet");
-            if (index >= 0 && args.Length > index + 2)
+            if (index >= 0 && args.Length >= index + 2)
             {
                 options.SheetName = args[index + 1];
             }
             index = args.IndexOf("-start");
-            if (index >= 0 && args.Length > index + 2)
+            if (index >= 0 && args.Length >= index + 2)
             {
                 options.StartColumn = args[index + 1];
             }
             index = args.IndexOf("-end");
-            if (index >= 0 && args.Length > index + 2)
+            if (index >= 0 && args.Length >= index + 2)
             {
                 options.LastColumn = args[index + 1];
             }
             index = args.IndexOf("-skip");
-            if (index >= 0 && args.Length > index + 2)
+            if (index >= 0 && args.Length >= index + 2)
             {
                 options.SkipColumns = args[index + 1].Split(',', StringSplitOptions.RemoveEmptyEntries);
             }
